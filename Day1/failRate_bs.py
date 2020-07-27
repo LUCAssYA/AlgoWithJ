a39326ab32ae2cfc7a7bcf771ea3e69235c13192
beoms <bm1478@naver.com>
2020-07-27 16:34:22 +0900

    failrate complete

--- Day1/failRate_bs.py
+++ Day1/failRate_bs.py
@@ -1,23 +1,28 @@
-def failrate(n, stages):
+def failrate(N, stages):
     stageing = []
     stagearrived = []
-    for i in range(n):
+    for i in range(N):
         stageing.append(0)
         stagearrived.append(0)
 
     for i in stages:
-        if i < n + 1:
+        if i < N + 1:
             stageing[i - 1] += 1
             for j in range(i):
                 stagearrived[j] += 1
-        elif i == n + 1:
-            for j in range(n):
+        elif i == N + 1:
+            for j in range(N):
                 stagearrived[j] += 1
 
     stagerate = {}
-    for i in range(n):
-        stagerate[i+1] = stageing[i]/stagearrived[i]
+    for i in range(N):
+        if stagearrived[i] != 0:
+            stagerate[i+1] = stageing[i]/stagearrived[i]
+        else:
+            stagerate[i+1] = 0
 
     sortedrate = dict(sorted(stagerate.items(), reverse=True, key=lambda x: x[1]))
     answer = list(sortedrate.keys())
     return answer
+
+print(failrate(8, [1,2,3,4,5,6,7]))
